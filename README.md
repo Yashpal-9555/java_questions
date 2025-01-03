ccec2c030e3d3a3019873f3892bc38b6df96bbe1
Yashpal-555 <kyash232002@gmail.com>
2025-01-03 12:59:07 +0530

    all

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # java_questions
+Name - Yash Kumar Pal 
+Adm no:- 22scse1012689
+Sec-23;
---
+++ file.java
@@ -0,0 +1,8 @@
+package java_questions;
+
+class solution{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("my name is yash ");
+    }
+}
